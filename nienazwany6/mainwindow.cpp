955540db7df0b49afd35dc08d8ea8a297cdab708
figaro123 <zaq1xsw2zaq1@wp.pl>
2013-11-14 17:22:28 +0100

    dwa
    jakis opis

--- nienazwany6/mainwindow.cpp
+++ nienazwany6/mainwindow.cpp
@@ -1,14 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    MainWindow *w1 = new MainWindow();
+    w1->show();
+
 }
